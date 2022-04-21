4daa16002607b0ad9d1f0ac86241b77746d56dda
boronspoon <rayanticlimatic@gmail.com>
2022-04-21 10:30:27 +0900

    nbody in progress

--- 04_simd/13_nbody.cpp
+++ 04_simd/13_nbody.cpp
@@ -11,14 +11,15 @@ int main() {
     m[i] = drand48();
     fx[i] = fy[i] = 0;
   }
+  __mm256 xvec = _mm256_load_ps(x);
+  __mm256 yvec = _mm256_load_ps(y);
   for(int i=0; i<N; i++) {
-    for(int j=0; j<N; j++) {
       if(i != j) {
-        float rx = x[i] - x[j];
-        float ry = y[i] - y[j];
-        float r = std::sqrt(rx * rx + ry * ry);
-        fx[i] -= rx * m[j] / (r * r * r);
-        fy[i] -= ry * m[j] / (r * r * r);
+        __mm256 rx = _mm_sub_ps(_mm_set1_ps(x[i]),x);
+        __mm256 ry = _mm_sub_ps(_mm_set1_ps(y[i]),y);
+        __mm256 recpr = _mm256_rsqrt_ps(_mm_mul_ps(rx,rx),_mm_mul_ps(ry,ry));
+        fx[i] -= rx * m[j] / recpr(r * r * r);
+        fy[i] -= ry * m[j] * _mm_mul_ps(_mm_mul_ps(recpr,recpr),recpr);
       }
     }
     printf("%d %g %g\n",i,fx[i],fy[i]);
