234bb4c44df62267961780663fc35353425232a3
rayanti <rayanticlimatic@gmail.com>
2022-06-10 22:16:43 +0900

    fix typo

--- 14_pde/10_cavity_mpi.cpp
+++ 14_pde/10_cavity_mpi.cpp
@@ -195,7 +195,7 @@ for (int n = 0; n < nt; n++) {/*
         }
     }
     if (rank == 0) {
-        printf("n:%d,rank:%d,b[41:46]=%.8e,%.8e,%.8e,%.8e,%.8e\n",n,rank,b[41],b[42],b[43],b[44],b[45]);
+        printf("n:%d,rank:%d,u[41:46]=%.8e,%.8e,%.8e,%.8e,%.8e\n",n,rank,u[41],u[42],u[43],u[44],u[45]);
     }/*
     MPI_Gatherv(&u[0], size-1, MPI_DOUBLE, &u0[0], counts, displacements, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     MPI_Gatherv(&v[0], size-1, MPI_DOUBLE, &v0[0], counts, displacements, MPI_DOUBLE, 0, MPI_COMM_WORLD);
